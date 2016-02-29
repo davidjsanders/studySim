3ad6ff06e40894bcf38958067b751295cfc7b699
dsandersAzure <dsanders_can@outlook.com>
2016-02-28 20:01:55 -0500

    Image Change

--- initialize.sh
+++ initialize.sh
@@ -3,6 +3,7 @@ echo "Pulling down latest container images in 5 seconds."
 echo "Press ^c to cancel"
 echo ""
 sleep 5
+docker pull dsanderscan/pythonbase
 docker pull dsanderscan/mscit_stage2_bluetooth
 docker pull dsanderscan/mscit_stage2_logger
 docker pull dsanderscan/mscit_stage2_log_viewer
