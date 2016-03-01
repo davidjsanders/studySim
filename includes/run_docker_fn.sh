efac817485c7cfe7dd7749894a71e8c51ab675a7
dsandersAzure <dsanders_can@outlook.com>
2016-02-29 20:36:03 -0500

    Update run_docker remove persistence

--- includes/run_docker_fn.sh
+++ includes/run_docker_fn.sh
@@ -5,6 +5,18 @@ function run_docker {
     echo -n "Starting service $2 (port $1 on $serverName): "
     docker run -p $1:$1 --name stage2_$2$1 \
         --net=isolated_nw -e portToUse=$1 -e serverName="$serverName" \
+        -e TZ=`date +%Z` \
+        -d dsanderscan/mscit_stage2_$2
+    sleep 1
+}
+
+function run_docker_persist {
+    # $1 - Port number
+    # $2 - Container image name
+    # $3 - Directory path
+    echo -n "Starting service $2 (port $1 on $serverName): "
+    docker run -p $1:$1 --name stage2_$2$1 \
+        --net=isolated_nw -e portToUse=$1 -e serverName="$serverName" \
         -e TZ=`date +%Z` -v $PWD/$3/datavolume:/$3/datavolume \
         -d dsanderscan/mscit_stage2_$2
     sleep 1
--- includes/run_docker_phone_fn.sh
+++ includes/run_docker_phone_fn.sh
@@ -6,6 +6,18 @@ function run_docker_phone {
         -e portToUse=$phonePort \
         -e serverName="$serverName" \
         -e TZ=`date +%Z` \
+        -d dsanderscan/mscit_stage2_phone -c /Phone/startup.sh \
+    sleep 1
+}
+
+function run_docker_phone_persist {
+    echo -n "Starting phone (port $phonePort on $serverName): "
+    docker run -p 16379:6379 -p $phonePort:$phonePort \
+        --net=isolated_nw \
+        --name stage2_phone$phonePort \
+        -e portToUse=$phonePort \
+        -e serverName="$serverName" \
+        -e TZ=`date +%Z` \
         -v $PWD/datavolume:/Phone/datavolume \
         -d dsanderscan/mscit_stage2_phone -c /Phone/startup.sh \
     sleep 1
