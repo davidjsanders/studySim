5206e66dbe1f8bd12cf50cd87a7c27100f0ed8e4
dsandersAzure <dsanders_can@outlook.com>
2016-03-02 15:17:55 -0500

    Add multi-stage param to run_docker

--- initialize.sh
+++ initialize.sh
@@ -8,7 +8,8 @@ docker pull dsanderscan/mscit_stage2_bluetooth
 docker pull dsanderscan/mscit_stage2_logger
 docker pull dsanderscan/mscit_stage2_log_viewer
 docker pull dsanderscan/mscit_stage2_monitor_app
-docker pull dsanderscan/mscit_stage2_phone
 docker pull dsanderscan/mscit_stage2_location_service
 docker pull dsanderscan/mscit_stage2_notification
+docker pull dsanderscan/mscit_stage2_phone
+docker pull dsanderscan/mscit_stage3_phone
 docker pull dsanderscan/mscit_stage2_phone_screen
