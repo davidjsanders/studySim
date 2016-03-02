ff721ea6bbd55cd8ba2c5cf61dd9245dda94cf2d
dsandersAzure <dsanders_can@outlook.com>
2016-03-02 14:42:01 -0500

    Add multi-stage param to run_docker

--- includes/general_ports.sh
+++ includes/general_ports.sh
@@ -1,9 +1,9 @@
-export loggerPort=100
-export notesvcPort=101
-export bluePort=102
-export monitorPort=103
-export locPort=104
-export phonePort=1080
+export loggerPort=43124
+export notesvcPort=43125
+export bluePort=43126
+export monitorPort=43127
+export locPort=43128
+export phonePort=43129
 export serverName=`hostname`
 #export serverIP="`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`"
 export serverIP="`ifconfig eth0 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`"
