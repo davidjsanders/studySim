50ab20c663b7908a36a019e27a094802efeeb0bb
dsandersAzure <dsanders_can@outlook.com>
2016-03-03 16:04:20 -0500

    Stage 3 - Phone - Context Checking

--- includes/shutdown.sh
+++
@@ -1,6 +0,0 @@
-function finalize {
-    let test_id=test_id+1
-    FILENAME="RUN-"$(date +%d%m%Y%H%M%S)".log"
-    pre_test $test_id "Saving log data to $FILENAME"
-    $simpath/show_log.sh > $FILENAME
-}
