55950de824c7484feb57c4fd37f52f04bec54bb9
dsandersAzure <dsanders_can@outlook.com>
2016-03-03 10:21:40 -0500

    Modified scripts to check docker status

--- includes/stop_phone_fn.sh
+++ includes/stop_phone_fn.sh
@@ -2,9 +2,15 @@ function stop_phone {
     #$1 - stage
     #$2 - port
     echo -n "Stopping phone screen ${2}: "
-    docker stop $1"phone_screen_"${2}
-    echo -n "Removing phone screen ${2}: "
-    docker rm -f $1"phone_screen_"${2}
+    check_docker $1"phone_screen_"${2}   # sets $DOCKER_CHECK
+    if [ "X" == "${DOCKER_CHECK}" ]; then
+        echo "already stopped."
+    else
+        docker stop $1"phone_screen_"${2}
+        echo -n "Removing phone screen ${2}: "
+        docker rm -f $1"phone_screen_"${2}
+        sleep 1
+    fi
     echo
 }
 
