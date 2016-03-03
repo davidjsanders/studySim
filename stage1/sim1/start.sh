64745f71f75c8a0448497d17574cf4ed73fc9319
dsandersAzure <dsanders_can@outlook.com>
2016-03-03 16:15:44 -0500

    Stage 3 - Phone - Context Checking

---
+++ stage1/sim1/start.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+#References
+# http://stackoverflow.com/questions/2924697/how-does-one-output-bold-text-in-bash
+# http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
+#
+#set -o verbose
+if [ "X"$simpath == "X" ]; then
+    echo "ERROR: simpath is not defined!"
+    echo ""
+    echo "Before running this script, ensure that simpath is defined:"
+    echo ""
+    echo "  export simpath=/path/to/studySim"
+    echo
+    exit 1
+fi
+
+source $simpath/includes/startup.sh
+source $simpath/includes/shutdown.sh
+source $simpath/includes/variables.sh
+source $simpath/includes/pause.sh
+source $simpath/includes/general_ports.sh
+source $simpath/includes/screen_decorations.sh
+source $simpath/includes/pre_test_fn.sh
+source $simpath/includes/post_test_fn.sh
+source $simpath/includes/curl_fn.sh
+source $simpath/includes/bolded_message_fn.sh
+
+test_id=0
+
+clear
+bolded_message "Test Set 1. Begins at $(date)"
+
+set +e
+# Get lock status of the phone
+let test_id=test_id+1
+do_get "" \
+       $phonePort \
+       "/v1_00/config/lock" \
+       "Get the current lock status of the phone" \
+       $test_id
+
+# Lock the phone
+let test_id=test_id+1
+export data=""
+do_post "${data}" \
+         $phonePort \
+         "/v1_00/config/lock" \
+         "Send an SMS Message to the phone" \
+         $test_id
+
+# Send an SMS Message to the phone
+let test_id=test_id+1
+export data='{'$phoneKey', "sender":"SMS", "message":"This is a text message received via SMS", "action":"open"}'
+do_post "${data}" \
+         $phonePort \
+         "/v1_00/notification" \
+         "Send an SMS Message to the phone" \
+         $test_id
+
+# Connect to Monitor App
+let test_id=test_id+1
+export monitor_app='"monitor-app":"'$serverIPName':'$monitorPort'/v1_00"'
+export service='"notification-service":"'$serverIPName':'$notesvcPort'/v1_00/notification"'
+export recipient='"recipient":"'$serverIPName':'$phonePort'/v1_00/notification"'
+export location='"location-service":"'$serverIPName':'$locPort'/v1_00/check"'
+export provider='"location-provider":"'$serverIPName':'$phonePort'/v1_00/location"'
+export data='{'$genKey', '$monitor_app', '$service', '$recipient', '$location', '$provider'}'
+do_post "${data}" \
+        $phonePort \
+        "/v1_00/config/monitor" \
+        "Connect to Monitor App. " \
+        $test_id
+
+# Configure Grindr as a monitored application
+let test_id=test_id+1
+export data='{'$genKey', "description":"Grindr is an app for men seeking men"}'
+do_post "${data}" \
+        $monitorPort \
+        "/v1_00/app/grindr" \
+        "Configure Grindr as a monitored application" \
+        $test_id
+
+# Validate Grindr is being monitored
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_get "${data}" \
+       $monitorPort \
+       "/v1_00/app/grindr" \
+       "Validate Grindr is being monitored" \
+        $test_id
+
+# Configure ManHunt as a monitored application
+let test_id=test_id+1
+export data='{'$genKey', "description":"ManHunt is a location based app for men seeking men"}'
+do_post "${data}" \
+        $monitorPort \
+        "/v1_00/app/manhunt" \
+        "Configure ManHunt as a monitored application" \
+        $test_id
+
+# Launch the Facebook client - A Notification will NOT be issued
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_post "${data}" \
+        $phonePort \
+        "/v1_00/config/launch/facebook" \
+        "Launch the Facebook client - A Notification will NOT be issued" \
+        $test_id
+
+# Launch Grindr - A Notification will be issued
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_post "${data}" \
+        $phonePort \
+        "/v1_00/config/launch/grindr" \
+        "Launch Grindr - A Notification will be issued" \
+        $test_id
+
+# Launch the phone mail client - A Notification will NOT be issued
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_post "${data}" \
+        $phonePort \
+        "/v1_00/config/launch/mailclient" \
+        "Launch the phone mail client - A Notification will NOT be issued" \
+        $test_id
+
+# Send a text message to the phone
+let test_id=test_id+1
+export data='{'$phoneKey', "sender":"SMS", "message":"Can you pick me up at Starbucks, please? Its the one at Clair and Gordon. Thanks John.", "action":"open"}'
+do_post "${data}" \
+        $phonePort \
+        "/v1_00/notification" \
+        "Send a text message to the phone" \
+        $test_id
+
+# Stop monitoring Grindr
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_delete "${data}" \
+          $monitorPort \
+          "/v1_00/app/grindr" \
+          "Stop monitoring Grindr" \
+          $test_id
+
+# Validate Grindr is no longer being monitored
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_get "${data}" \
+       $monitorPort \
+       "/v1_00/app/grindr" \
+       "Validate Grindr is no longer being monitored. Monitor_App returns 404" \
+        $test_id
+
+# Launch Grindr - A Notification will NOT be issued
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_post "${data}" \
+        $phonePort \
+        "/v1_00/config/launch/grindr" \
+        "Launch Grindr - A Notification will NOT be issued" \
+        $test_id
+
+# Disconnect the Monitor App
+let test_id=test_id+1
+export data='{'$genKey'}'
+do_delete "${data}" \
+           $phonePort \
+           "/v1_00/config/monitor" \
+           "Connect to Monitor App. " \
+           $test_id
+
+# Save logs
+finalize
+
+bolded_message "Tests end at $(date)"
+
+
---
+++ stage1/sim6/setup.sh
@@ -0,0 +1,71 @@
+if [ "X"$simpath == "X" ]; then
+    echo "ERROR: simpath is not defined!"
+    echo ""
+    echo "Before running this script, ensure that simpath is defined:"
+    echo ""
+    echo "  export simpath=/path/to/studySim"
+    echo
+    exit 1
+fi
+
+source $simpath/includes/startup.sh
+source $simpath/includes/variables.sh
+source $simpath/includes/general_ports.sh
+source $simpath/includes/config_logging_fn.sh
+source $simpath/includes/run_docker_fn.sh
+source $simpath/includes/run_docker_phone_fn.sh
+source $simpath/includes/start_phone_fn.sh
+source $simpath/includes/screen_decorations.sh
+source $simpath/includes/pre_test_fn.sh
+source $simpath/includes/post_test_fn.sh
+source $simpath/includes/curl_fn.sh
+source $simpath/includes/bolded_message_fn.sh
+
+echo
+set +e
+bolded_message "Setup for Test Set 2. Begins at $(date)"
+set -e
+
+source $simpath/includes/validate_docker_network.sh
+
+run_docker "stage2_" $loggerPort "logger" "Logger"
+sleep 2
+
+do_delete '{'$genKey'}' $loggerPort '/v1_00/log' "Clear logs."
+sleep 1
+
+run_docker_persist "stage2_" $bluePort "bluetooth" "Bluetooth"                   # Bluetooth
+run_docker "stage2_" $locPort "location_service" "Location_Service"      # Location Service
+run_docker "stage2_" $monitorPort "monitor_app" "Monitor_App"            # Monitor App
+run_docker "stage2_" $notesvcPort "notification" "Notification_Service"  # Notification Service
+run_docker_phone_persist "stage3_"                                               # Start the phone
+
+echo ""
+echo -n "Pausing to let services complete start-up: "
+sleep 2
+echo "done."
+
+echo ""
+echo "${underline}Starting phone screens${normal}"
+echo ""
+
+# Setup Jing to be able to see the phone
+start_phone "stage2_" Jing
+
+# Setup Bob to be able to see the phone
+start_phone "stage2_" Bob
+
+echo ""
+echo "${underline}Configure logging.${normal}"
+echo ""
+config_logging $bluePort "Bluetooth"                 # Bluetooth
+config_logging $locPort "Location Service"           # Location Service
+config_logging $monitorPort "Monitor App"            # Monitor App
+config_logging $notesvcPort "Notification Service"   # Notification Service
+config_logging $phonePort "Phone"                    # Phone
+echo ""
+echo "Logging configured."
+echo ""
+set +e
+bolded_message "Setup for Test Set 2. Ends at $(date)."
+echo
