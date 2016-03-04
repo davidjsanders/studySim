08282b6c3a64f3bdb5b24fe495eeac17b5adc227
dsandersAzure <dsanders_can@outlook.com>
2016-03-04 15:40:41 -0500

    Update pause_fn.sh - fix delay at end.

--- stage2/includes/pause_fn.sh
+++ stage2/includes/pause_fn.sh
@@ -4,10 +4,9 @@ function pause {
     #echo "Ensure you have run setup before running this simulation"
     echo -n "${1}begins in...5"
     while [ $COUNTER -gt 1 ]; do
+        sleep 1
         let COUNTER=COUNTER-1
         echo -n ", "$COUNTER
-        sleep 1
     done
-    sleep 1
     echo ". Starting."
 }
