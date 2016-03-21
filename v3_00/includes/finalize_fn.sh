ea0a766c398f553dfd58f0fc1e4717e7f1c92bbe
dsandersAzure <dsanders_can@outlook.com>
2016-03-21 16:48:12 -0400

    Fix v3_00 show_log.sh

--- v3_00/includes/finalize_fn.sh
+++ v3_00/includes/finalize_fn.sh
@@ -3,6 +3,6 @@ function finalize {
     filename="RUN-"$(date +%d%m%Y%H%M%S)".log"
     pre_test $test_id "Saving log data to $filename"
     pre_test $test_id ""$simpath"/"$version"/show_log.sh --logger "$serverIPName":"$loggerPort"/"$version"/log" 
-    $simpath/$presentAs/show_log.sh --logger $serverIPName:$loggerPort/$presentAs/log > $filename
+    $simpath/$version/show_log.sh --logger $serverIPName:$loggerPort/$presentAs/log > $filename
     echo
 }
