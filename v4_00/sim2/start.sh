5fbbcc4cc2f1fe71cefe140be1d6864b292218a3
dsandersAzure <dsanders_can@outlook.com>
2016-04-08 21:40:00 -0400

    v4_00 - Sim2

--- v4_00/sim2/start.sh
+++ v4_00/sim2/start.sh
@@ -44,220 +44,95 @@ do_post "${data}" \
         "Add a user David to the presence engine, with status available" \
         $test_id
 
-exit 0
 
-# Turn autosense on
+# Configure the door bell to recognize David
 let test_id=test_id+1
-data='{'$genKey'}'
+first_name='"first-name":"David"'
+family_name='"family-name":"Sanders"'
+notification_service='"notification-service":"'$serverIPName':'$notesvcPort'/'$presentAs'/notification"'
+call_back='"call-back":"'$serverIPName':'$phonePort'/'$presentAs'/notification"'
+call_back_key='"call-back-key":"1234-5678-9012-3456"'
+data='{'$genKey', '$notification_service', '$first_name', '$family_name', '$call_back', '$call_back_key'}'
 do_post "${data}" \
-          $phonePort \
-          "/"$presentAs"/config/auto-sense" \
-          "Set the phone auto sense feature to on to check profanity, etc." \
+          $doorbellPort \
+          "/"$presentAs"/occupant/david" \
+          "Configure the door to recognize David (david) as an occupant." \
           $test_id
 
-# lock the phone
+
+# Configure the phone to use the presence engine
 let test_id=test_id+1
-data='{'$genKey'}'
+presence_engine='"presence-engine":"'$serverIPName':'$presencePort'/'$presentAs'/user"'
+data='{'$presence_engine'}'
 do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/lock" \
-        "Lock the phone" \
+        $doorbellPort \
+        "/"$presentAs"/config/presence" \
+        "Configure the phone to use the presence engine" \
         $test_id
 
-# Set the phone to show notifications when locked
+
+# Delivery man rings the door bell (Phone 2)
 let test_id=test_id+1
-data='{'$genKey'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/show-on-lock" \
-        "Set the phone to show notifications when locked" \
+call_back='"call-back":"'$serverIPName':'$phone3Port'/'$presentAs'/notification"'
+call_back_key='"call-back-key":"1234-5678-9012-3456"'
+call_from='"call-from":"FedEx"'
+data='{'$call_back', '$call_back_key', '$call_from'}'
+do_put "${data}" \
+        $doorbellPort \
+        "/"$presentAs"/ring/david" \
+        "Delivery man rings the door bell for David (Phone 2)" \
         $test_id
 
-# Send an SMS Message to the phone
+# Set David's presence to busy
 let test_id=test_id+1
-recipient='"recipient":"'$serverIPName':'$phonePort'/'$presentAs'/notification"'
-sender='"sender":"SMS"'
-action='"action":"Read Text"'
-urgency='"urgency":false'
-message='"message":"This is a text message received via SMS"'
-data='{'$genKey', '$urgency', '$recipient', '$sender', '$action', '$message'}'
+userstatus='"status":"busy"'
+data='{'$userstatus'}'
 do_post "${data}" \
-         $notesvcPort \
-         "/"$presentAs"/notification" \
-         "Send an SMS Message to the phone marked non-urgent" \
-         $test_id
-
-# Set a context privacy state
-let test_id=test_id+1
-state='with_others'
-value='"value":"TRUE"'
-ttl='"time-to-live":10000'
-data='{'$genKey', '$value', '$ttl'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/state/"$state \
-        "Set a context state: with others = True. TTL is 10,000 seconds for testing" \
+        $presencePort \
+        "/"$presentAs"/user/david" \
+        "Add a user David to the presence engine, with status available" \
         $test_id
 
-# Connect to Monitor App
+
+# Delivery man rings the door bell (Phone 2)
 let test_id=test_id+1
-monitor_app='"monitor-app":"'$serverIPName':'$monitorPort'/'$presentAs'"'
-service='"notification-service":"'$serverIPName':'$notesvcPort'/'$presentAs'/notification"'
-recipient='"recipient":"'$serverIPName':'$phonePort'/'$presentAs'/notification"'
-location='"location-service":"'$serverIPName':'$locPort'/'$presentAs'/check"'
-provider='"location-provider":"'$serverIPName':'$phonePort'/'$presentAs'/location"'
-data='{'$genKey', '$monitor_app', '$service', '$recipient', '$location', '$provider'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/monitor" \
-        "Connect to Monitor App. " \
+call_back='"call-back":"'$serverIPName':'$phone3Port'/'$presentAs'/notification"'
+call_back_key='"call-back-key":"1234-5678-9012-3456"'
+call_from='"call-from":"FedEx"'
+data='{'$call_back', '$call_back_key', '$call_from'}'
+do_put "${data}" \
+        $doorbellPort \
+        "/"$presentAs"/ring/david" \
+        "Delivery man rings the door bell for David (Phone 2)" \
         $test_id
-
-# Configure Grindr as a monitored application
-let test_id=test_id+1
-data='{'$genKey', "description":"Grindr is an app for men seeking men"}'
-do_post "${data}" \
-        $monitorPort \
-        "/"$presentAs"/app/grindr" \
-        "Configure Grindr as a monitored application" \
-        $test_id
-
-# Validate Grindr is being monitored
-let test_id=test_id+1
-data='{'$genKey'}'
-do_get "${data}" \
-       $monitorPort \
-       "/"$presentAs"/app/grindr" \
-       "Validate Grindr is being monitored" \
-        $test_id
-
-# Configure ManHunt as a monitored application
-let test_id=test_id+1
-data='{'$genKey', "description":"ManHunt is a location based app for men seeking men"}'
-do_post "${data}" \
-        $monitorPort \
-        "/"$presentAs"/app/manhunt" \
-        "Configure ManHunt as a monitored application" \
-        $test_id
-
-# Launch the Facebook client - A Notification will NOT be issued
-let test_id=test_id+1
-data='{'$genKey'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/launch/facebook" \
-        "Launch the Facebook client - A Notification will NOT be issued" \
-        $test_id
-
-# Launch Grindr - A Notification will be issued
-let test_id=test_id+1
-data='{'$genKey'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/launch/grindr" \
-        "Launch Grindr - A Notification will be issued" \
-        $test_id
-
-# Launch the phone mail client - A Notification will NOT be issued
-let test_id=test_id+1
-data='{'$genKey'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/launch/mailclient" \
-        "Launch the phone mail client - A Notification will NOT be issued" \
-        $test_id
-
-# Send an SMS Message to the phone
-let test_id=test_id+1
-recipient='"recipient":"'$serverIPName':'$phonePort'/'$presentAs'/notification"'
-sender='"sender":"SMS"'
-action='"action":"Read Text"'
-message='"message":"Can you profanity pick me up at Starbucks, please? Thanks John."'
-data='{'$genKey', '$recipient', '$sender', '$action', '$message'}'
-do_post "${data}" \
-         $notesvcPort \
-         "/"$presentAs"/notification" \
-         "Send an SMS Message to the phone" \
-         $test_id
-
-# Stop monitoring Grindr
-let test_id=test_id+1
-data='{'$genKey'}'
-do_delete "${data}" \
-          $monitorPort \
-          "/"$presentAs"/app/grindr" \
-          "Stop monitoring Grindr" \
-          $test_id
-
-# Validate Grindr is no longer being monitored
-let test_id=test_id+1
-data='{'$genKey'}'
-do_get "${data}" \
-       $monitorPort \
-       "/"$presentAs"/app/grindr" \
-       "Validate Grindr is no longer being monitored. Monitor_App returns 404" \
-        $test_id
-
-# Launch Grindr - A Notification will NOT be issued
-let test_id=test_id+1
-data='{'$genKey'}'
-do_post "${data}" \
-        $phonePort \
-        "/"$presentAs"/config/launch/grindr" \
-        "Launch Grindr - A Notification will NOT be issued" \
-        $test_id
-
-# Bob can no longer see the screen
-let test_id=test_id+1
-pre_test $test_id "Bob leaves and can no longer see the phone screen."
-stop_phone Bob
-
-# Pause for 5 seconds to let the notification be detected
-let test_id=test_id+1
-pre_test $test_id "Sleeping for 5 seconds to persist notifications."
-sleep 5
-echo
-
-# Remove the context privacy state
-let test_id=test_id+1
-state='with_others'
-data='{'$genKey'}'
-do_delete "${data}" \
-          $phonePort \
-          "/"$presentAs"/config/state/"$state \
-          "Remove the context state 'with others' now Bob has left" \
-          $test_id
-
-# Unlock the phone
-#let test_id=test_id+1
-#data='{'$genKey'}'
-#do_put "${data}" \
-#       $phonePort \
-#       "/"$presentAs"/config/unlock" \
-#       "Unlock the phone" \
-#       $test_id
-
-# Pause for 5 seconds to let the notification be detected
-let test_id=test_id+1
-pre_test $test_id "Sleeping for 5 seconds to allow notifications to be detected."
-sleep 5
-echo
-
-# Disconnect the Monitor App
-let test_id=test_id+1
-data='{'$genKey'}'
-do_delete "${data}" \
-           $phonePort \
-           "/"$presentAs"/config/monitor" \
-           "Disconnect from Monitor App. " \
-           $test_id
 
 
 # Get the phone's screen
 let test_id=test_id+1
-screen_filename="screen-"$(date +%d-%m-%Y-%H%M%S)".txt"
-pre_test $test_id "Downloading phone main screen saving to ${screen_filename}"
+screen_filename="david-screen-"$(date +%d-%m-%Y-%H%M%S)".txt"
+pre_test $test_id "Downloading David's phone screen saving to ${screen_filename}"
 wget $serverIPName:$phonePort/$presentAs/config/screen -O "${screen_filename}"
+echo
+
+# Get the phone's screen
+let test_id=test_id+1
+screen_filename="friend-screen-"$(date +%d-%m-%Y-%H%M%S)".txt"
+pre_test $test_id "Downloading friend phone screen saving to ${screen_filename}"
+wget $serverIPName:$phone2Port/$presentAs/config/screen -O "${screen_filename}"
+echo
+
+# Get the phone's screen
+let test_id=test_id+1
+screen_filename="fedex-screen-"$(date +%d-%m-%Y-%H%M%S)".txt"
+pre_test $test_id "Downloading delivery man's phone screen saving to ${screen_filename}"
+wget $serverIPName:$phone3Port/$presentAs/config/screen -O "${screen_filename}"
+echo
+
+# Get the phone's screen
+let test_id=test_id+1
+screen_filename="stranger-screen-"$(date +%d-%m-%Y-%H%M%S)".txt"
+pre_test $test_id "Downloading stranger's phone screen saving to ${screen_filename}"
+wget $serverIPName:$phone4Port/$presentAs/config/screen -O "${screen_filename}"
 echo
 
 # Get the central log file
