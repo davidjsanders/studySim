bcbf47eefce47051b4e65a46d1b32a8f3cd392c9
dsandersAzure <dsanders_can@outlook.com>
2016-04-10 08:54:16 -0400

    v4_00 Remove bold, underline, and normal

--- includes/screen_decorations.sh
+++ includes/screen_decorations.sh
@@ -1,5 +1,8 @@
-export bold=$(tput rev)
-export underline=$(tput smul)
-export normal=$(tput sgr0)
+export bold=""
+export underline=""
+export normal=""
+#export bold=$(tput rev)
+#export underline=$(tput smul)
+#export normal=$(tput sgr0)
 
 
