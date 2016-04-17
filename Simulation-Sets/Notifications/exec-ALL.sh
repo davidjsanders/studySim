90fb337b06036ec8ec2b3b597a3585e184a98e0e
dsandersAzure <dsanders_can@outlook.com>
2016-04-17 13:02:13 -0400

    Simulation Set 1 - exec-ALL.sh

---
+++ Simulation-Sets/Notifications/exec-ALL.sh
@@ -0,0 +1,31 @@
+script_array=("1-Sending-Texts-Raising-Notifications" \
+              "2-Hot-Spot-Notifications" \
+              "3-Bluetooth-Sending-Texts-Raising-Notifications" \
+              "4-Bluetooth-Hot-Spot-Notifications" \
+              )
+model_sets=("1-no-context-start" \
+            "2-obfuscation-start" \
+            "3-obfuscation-lock-start" \
+            "4-context-start"
+           )
+
+# array lenght is number of items in the array, so -1 for [0..n]
+simulation_name="Notifications"
+model_set_length=${#model_sets[*]}
+
+top_header_line=$(printf "%0.s=" {1..77})
+
+let actual_length=(model_set_length-1)
+
+# Loop through the array and execute the simulations
+for i in `seq 0 1 ${actual_length}`;
+do
+    let vtu=(i+1)   # vtu = verstion to use
+    echo
+    echo ${top_header_line}
+    echo "* Starting ${model_sets[$i]} at "$(date +"%H:%M:%S")
+    echo ${top_header_line}
+    $simpath/Simulation-Sets/${simulation_name}/exec-model-${vtu}.sh "-v v"${vtu}"_00"
+    echo
+done
+
